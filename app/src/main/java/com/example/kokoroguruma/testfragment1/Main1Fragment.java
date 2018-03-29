98ed0e180b522bd72324a61a634639f37fda3e48
Kokoroguruma <kokoroguruma@hotmail.com>
2018-03-29 17:38:44 +0900

    完成

--- app/src/main/java/com/example/kokoroguruma/testfragment1/Main1Fragment.java
+++ app/src/main/java/com/example/kokoroguruma/testfragment1/Main1Fragment.java
@@ -1,11 +1,17 @@
 package com.example.kokoroguruma.testfragment1;
 
 
+import android.content.Context;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +19,115 @@ import android.view.ViewGroup;
  */
 public class Main1Fragment extends Fragment {
 
-
-    public Main1Fragment() {
-        // Required empty public constructor
-    }
+	// タグの設定用。
+	public static final String TAG = Main1Fragment.class.getSimpleName();
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main1, container, false);
-    }
+
+	private MainActivity mainActivity = null;
+
+
+	public Main1Fragment() {
+		// Required empty public constructor
+	}
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+
+
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_main1, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+		this.button1Click();
+		this.button2Click();
+
+
+		super.onViewCreated(view, savedInstanceState);
+	}
+
+	private void button1Click() {
+		Button button = getView().findViewById(R.id.fragmentButton1);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				Log.d("Main1Fragment", "一応のnullチェック");
+				if (main1FragmentListener != null) {
+					Log.d("Main1Fragment", "MainActivityに実装されたEvent1()を実行する。");
+					main1FragmentListener.onMain1FragmentEvent1();
+				}
+
+			}
+		});
+	}
+
+	private void button2Click() {
+		Button button = getView().findViewById(R.id.fragmentButton2);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				Log.d("Main1Fragment", "一応のnullチェック");
+				if (main1FragmentListener != null) {
+					Log.d("Main1Fragment", "MainActivityに実装されたEvent2()を実行する。");
+					main1FragmentListener.onMain1FragmentEvent2();
+				}
+
+			}
+		});
+	}
+
+	/**
+	 * Activityから呼び出された時の関連付け
+	 *
+	 * @param context
+	 */
+	@Override
+	public void onAttach(Context context) {
+		Log.d("Main1Fragment", "Activityから呼び出されて関連付け開始");
+
+		if (context instanceof MainActivity) {
+			Log.d("Main1Fragment", "MainActivityの時の関連付け");
+			this.mainActivity = (MainActivity) context;
+
+			Log.d("Main1Fragment", "MainActivityの時の関連付け（Listener用）");
+			main1FragmentListener = (Main1FragmentListener) context;
+		}
+		super.onAttach(context);
+	}
+
+
+	// ここから「FragmentからActivityへの操作」
+	private Main1FragmentListener main1FragmentListener = null;
+
+	// インターフェースで動作があることを宣言する。
+	// Activityでimplementsする。
+	public interface Main1FragmentListener {
+		void onMain1FragmentEvent1();
+
+		void onMain1FragmentEvent2();
+	}
+
+
+
+
+	// ここから「ActivityからFragmentへの操作」
+
+	public void fragmentEvent1() {
+		TextView textView = getView().findViewById(R.id.fragmentTextView);
+		textView.setText("Event1");
+	}
+
+	public void fragmentEvent2() {
+		TextView textView = getView().findViewById(R.id.fragmentTextView);
+		textView.setText("Event2");
+	}
+
 
 }
--- app/src/main/java/com/example/kokoroguruma/testfragment1/MainActivity.java
+++ app/src/main/java/com/example/kokoroguruma/testfragment1/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.kokoroguruma.testfragment1;
 
+
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Main1Fragment.Main1FragmentListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		this.button1Click();
+		this.button2Click();
+
+	}
+
+	// Main1FragmentListenerより
+	@Override
+	public void onMain1FragmentEvent1() {
+		TextView textView = findViewById(R.id.mainTextView);
+		textView.setText("Event1()");
+	}
+
+	// Main1FragmentListenerより
+	@Override
+	public void onMain1FragmentEvent2() {
+		TextView textView = findViewById(R.id.mainTextView);
+		textView.setText("Event2()");
+	}
+
+
+	// ここから Activity のボタン。フラグメントの操作を行う。
+
+	private void button1Click() {
+		Button button = findViewById(R.id.mainButton1);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				Log.d("MainActivity", "button1: Click");
+
+				// フラグメントのアクセス先を確保。
+				FragmentManager manager = getSupportFragmentManager();
+				Main1Fragment main1Fragment = (Main1Fragment) manager.findFragmentById(R.id.mainFragment);
+				Log.d("MainActivity", "button1: main1Fragment: " + main1Fragment);
+
+				main1Fragment.fragmentEvent1();
+
+
+			}
+		});
+	}
+
+	private void button2Click() {
+		Button button = findViewById(R.id.mainButton2);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				Log.d("MainActivity", "button2: Click");
+
+				// フラグメントのアクセス先を確保。
+				FragmentManager manager = getSupportFragmentManager();
+				Main1Fragment main1Fragment = (Main1Fragment) manager.findFragmentById(R.id.mainFragment);
+
+				Log.d("MainActivity", "button2: main1Fragment: " + main1Fragment);
+
+				main1Fragment.fragmentEvent2();
+
+			}
+		});
+	}
+
+
 }
